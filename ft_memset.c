bf2fb580c9adcda30ca648e1bec0227eef71bd14
Caroline Sclavon <csclavon@c2r1p2.42nice.fr>
2024-04-25 14:01:04 +0200

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: csclavon <csclavon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 10:21:17 by csclavon          #+#    #+#             */
-/*   Updated: 2024/04/22 16:59:54 by csclavon         ###   ########.fr       */
+/*   Updated: 2024/04/25 14:00:36 by csclavon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void *ft_memset(void *s, int c, size_t n)
 {
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return;
+	while (i < n && s)
+	{
+		(unsigned char *)s[i] = (unsigned char)c;
+		i++;
+	}
 	
 }
