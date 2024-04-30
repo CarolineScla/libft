c6206403eeb4750c2c08c4c1e83c61fa0dd20a49
Caroline Sclavon <csclavon@c3r1p4.42nice.fr>
2024-04-30 18:59:53 +0200

    atoi memmove strlcat KO

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: csclavon <csclavon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:29:42 by csclavon          #+#    #+#             */
-/*   Updated: 2024/04/29 11:55:34 by csclavon         ###   ########.fr       */
+/*   Updated: 2024/04/30 18:53:41 by csclavon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,19 @@ int	ft_atoi(const char *nptr)
 	result = 0;
 	sign = 1;
 	i = 0;
-	while (nptr[i] == ' ' || nptr[i] == '+')
+	while (nptr[i] == ' ' || nptr[i] == '+' || (nptr[i] >= '\a' && nptr[i] <= '\r'))
+		i++;
+	if (nptr[i] == '-')
 	{
-		if (nptr[i] == '-')
-			sign *= -1;
+		sign *= -1;
 		i++;
 	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	while (!nptr[i])
 	{
+		if (nptr[i] >= '0' && nptr[i] <= '9')
+			result = result * 10 + nptr[i] - '0';
+		else
+			break;
 		i++;
 	}
 	return (sign * result);
